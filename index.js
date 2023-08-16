0c6bc8be01c4e53522276bb8b9c49f34886a9fe0
Justin Bobrick <j.bobrick@outlook.com>
2023-08-16 16:06:40 -0700

    added the license

--- index.js
+++ index.js
@@ -29,36 +29,51 @@ inquirer
         name: "credits",
         message: "Please provide any collaborators or third-party assets used:"
     },
-    {
-        type: "input",
-        name: "license",
-        message: "Please provide the license associated with your project:"
-    },
 ])
 .then((answers) => {
     fs.writeFile(
       "README.md",
       `## Description
-      ${answers.description}
+${answers.description}
 
-      ## Table of Contents
-      - [Description](#description)
-      - [Installation](#installation)
-      - [Usage](#usage)
-      - [Credits](#credits)
-      - [License](#license)
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
       
-      ## Installation
-      ${answers.installation}
+## Installation
+${answers.installation}
       
-      ## Usage
-      ${answers.usage}
+## Usage
+${answers.usage}
       
-      ## Credits
-      ${answers.credits}
+## Credits
+${answers.credits}
       
-      ## License
-      ${answers.license}`,
+## License
+MIT License
+
+Copyright (c) 2023 Justin Bobrick
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`,
       (err) => {
         if (err) {
           console.error(err);
